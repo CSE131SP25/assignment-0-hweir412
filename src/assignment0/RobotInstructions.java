77ce48ec281449da09d018dd55ad6e22a47bd276
haileyweir <haileyweir@localhost>
2025-01-17 13:46:12 -0600

    assignment 0 Trident!

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -36,6 +36,30 @@ public class RobotInstructions
 		robot.forward(23);
 		robot.turnRight(90);
 		robot.forward(80);
+		robot.turnRight(90);
+		robot.forward(20);
+		robot.turnRight(90);
+		robot.forward(60);
+		robot.turnLeft(90);
+		robot.forward(20);
+		robot.turnLeft(90);
+		robot.forward(60);
+		robot.turnRight(90);
+		robot.forward(20);
+		robot.turnRight(90);
+		robot.forward(60);
+		robot.turnLeft(90);
+		robot.forward(20);
+		robot.turnLeft(90);
+		robot.forward(60);
+		robot.turnRight(90);
+		robot.forward(20);
+		robot.turnRight(90);
+		robot.forward(80);
+		robot.turnRight(90);
+		robot.forward(23);
+		robot.turnLeft(90);
+		robot.forward(60);
 	}
 
 }
